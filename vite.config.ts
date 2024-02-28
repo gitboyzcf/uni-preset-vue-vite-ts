ffed99b089873c1dc569ac17efe82a490a4b2aae
gitboyzcf <boyzcf@qq.com>
2024-02-28 11:00:20 +0800

    main: Added init

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import path from 'path'
+import uni from "@dcloudio/vite-plugin-uni";
+import AutoImport from 'unplugin-auto-import/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [uni(),
+  AutoImport({
+    imports: ['vue', 'uni-app']
+  })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+});
