0680f59a4a3a43047923eb5bf1e9b1db8d85422b
gitboyzcf <boyzcf@qq.com>
2024-03-11 11:41:02 +0800

    测试

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,12 +1,12 @@
 import { defineStore } from 'pinia'
 export const useCountStore = defineStore('count', {
   state: () => ({
-    count: 0
+    count: 0,
   }),
   actions: {
     increment() {
       this.count++
-    }
+    },
   },
-  persist: true
+  persist: true,
 })
